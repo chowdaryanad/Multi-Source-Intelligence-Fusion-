f272faef7529ce41d2b37d27b5ee0e33dc8c0f52
anand <nalluriramakrishna762@gmail.com>
2026-04-19 17:56:22 +0530

    fix routes init imports

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,4 +1,4 @@
-from routes.data_routes import router as data_router
-from routes.image_routes import router as image_router
+from backend.routes.data_routes import router as data_router
+from backend.routes.image_routes import router as image_router
 
 __all__ = ["data_router", "image_router"]
